707590966406efd53fa8591495e1a9add5e3602f
brackebusch <kyle.brackebusch@gmail.com>
2018-02-20 10:08:39 -0800

    cleaned up files to reflect single working project

--- README.md
+++ README.md
@@ -106,3 +106,25 @@ Frontend: REact/JavaScript
 | date | date 	(ISO 8601)| 
 | start_time | time (ISO 8601)|
 | end_time | time (ISO 8601)|
+
+## Project Structure
+```
+|-- server/
+|  |-- server.js                            // The entry point for running the backend server locally, and main server for production
+|  |-- passport/                             // Configuration files used to connect to different machines or set settings
+|     |-- index.js                     // Overloads the passport object and defines serialize and deserialize
+|     |-- localStrategy.js            // Defines a local strategy
+|     |-- googleStrategy.js           // Defines google OAuth stratgey
+|     ....
+|  |-- db/                             
+|     |-- index.js                  // Configures the connection to the database
+|     |-- models/                   // represents data from our database, and defines schemas for each collection
+|        |-- user.js                // Schema for the User collection
+| -- src/                           // Entry for the React client side application
+```
+
+## Note
+* In order to set the google authentication up, you must register your app @ [https://console.developers.google.com](https://console.developers.google.com) & set `GOOGLE_CLIENT_ID` & `GOOGLE_CLIENT_SECRET` as environmental variables
+* In development mode (i.e. `npm run dev`), OAuth google callback is not being proxied to the google servers. Therefore in order to test the google OAuth on your local machine do the following:
+1) `npm run build`
+2) `npm run prod`
---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+
+class SignupForm extends Component {
+	constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: '',
+			confirmPassword: '',
+			redirectTo: null
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+		this.closeModal = this.closeModal.bind(this)
+	}
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+
+	handleSubmit(event) {
+		event.preventDefault()
+		// TODO - validate!
+		axios
+			.post('/auth/signup', {
+				username: this.state.username,
+				password: this.state.password
+			})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('youre good')
+					this.setState({
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('duplicate')
+				}
+			})
+	}
+
+	closeModal(event) {
+		this.setState({
+			redirectTo: '/'
+		})
+	}
+	render() {
+		if (this.state.redirectTo) {
+			return <Redirect to={{ pathname: this.state.redirectTo }} />
+		}
+		return (
+			<div className="session-form" onClick={this.closeModal}>
+				<div className="SignupForm">
+					<h1>Signup form</h1>
+					<label htmlFor="username">Username: </label>
+					<input
+						type="text"
+						name="username"
+						value={this.state.username}
+						onChange={this.handleChange}
+					/>
+					<label htmlFor="password">Password: </label>
+					<input
+						type="password"
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					<label htmlFor="confirmPassword">Confirm Password: </label>
+					<input
+						type="password"
+						name="confirmPassword"
+						value={this.state.confirmPassword}
+						onChange={this.handleChange}
+					/>
+					<button onClick={this.handleSubmit}>Sign up</button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default SignupForm
