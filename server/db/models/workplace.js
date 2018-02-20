45d4790a89af75f2d245f031722659f18616ae46
brackebusch <kyle.brackebusch@gmail.com>
2018-02-20 11:49:04 -0800

    resolved schema issues

--- server/db/models/workplace.js
+++ server/db/models/workplace.js
@@ -6,7 +6,7 @@ const workplaceSchema = new Schema({
 	workplaceName: { type: String, unique: false },
   placeId: { type: String, unique: true },
   shifts: { type : Array , "default" : [] },
-	employees: [{ type : ObjectId, ref: 'User' }]
+	employees: [{ type : Schema.ObjectId, ref: 'User' }]
 })
 
 // Define schema methods
@@ -18,4 +18,18 @@ workplaceSchema.methods = {
 
 // Create reference to Workpalce & export
 const Workplace = mongoose.model('Workpalce', workplaceSchema)
+
+var RedRobin = new Workplace({
+	workplaceName: "Red Robin",
+	placeId: "randomIDgoesHere",
+	shifts: [1,2,3,4]
+})
+
+// RedRobin.save(function(error) {
+//      console.log("Your bee has been saved!");
+//  if (error) {
+//      console.error(error);
+//   }
+//  });
+
 module.exports = Workplace
