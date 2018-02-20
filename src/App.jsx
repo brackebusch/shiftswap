f4703e266accbbe9806533966967da689064bb0f
Dylan <dmccapes@mac.com>
2018-02-20 10:41:21 -0800

    Change App, Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,18 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Route, Link } from 'react-router-dom'
-import './styling/App.css'
-import './styling/welcome.css'
-import LoginForm from './components/Login/LoginForm'
-import SignupForm from './components/SignupForm'
-import Header from './components/Header'
-import Home from './components/Home'
-
-const DisplayLinks = props => {
-	if (props.loggedIn) {
-		return (
-			<nav className="navbar">
-				<ul className="nav">
-					<div className="left-nav">
-						<li className="nav-item">
-							<h1>
-								ShiftSwap
-							</h1>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-link">
-								Home
-							</Link>
-						</li>
-					</div>
-					<div className="right-nav">
-						<li>
-							<Link to="#" className="nav-link" onClick={props._logout}>
-								Logout
-							</Link>
-						</li>
-					</div>
-				</ul>
-			</nav>
-		)
-	} else {
-		return (
-			<nav className="navbar">
-				<ul className="nav">
-					<div className="left-nav">
-						<li className="nav-item">
-							<h1>
-								ShiftSwap
-							</h1>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-link">
-								Home
-							</Link>
-						</li>
-					</div>
-					<div className="right-nav">
-						<li className="nav-item">
-							<Link to="/login" className="nav-link">
-								login
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/signup" className="nav-link">
-								sign up
-							</Link>
-						</li>
-					</div>
-				</ul>
-			</nav>
-		)
-	}
-}
+import React, { Component } from 'react';
+import NavBar from './components/NavBar.jsx';
+import Home from './components/Home.jsx';
+import './styling/App.css';
+import './styling/welcome.css';
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			loggedIn: false,
-			user: null
-		}
-		this._logout = this._logout.bind(this)
-		this._login = this._login.bind(this)
-	}
-	componentDidMount() {
-		axios.get('/auth/user').then(response => {
-			console.log(response.data)
-			if (!!response.data.user) {
-				console.log('THERE IS A USER')
-				this.setState({
-					loggedIn: true,
-					user: response.data.user
-				})
-			} else {
-				this.setState({
-					loggedIn: false,
-					user: null
-				})
-			}
-		})
-	}
-
-	_logout(event) {
-		event.preventDefault()
-		console.log('logging out')
-		axios.post('/auth/logout').then(response => {
-			console.log(response.data)
-			if (response.status === 200) {
-				this.setState({
-					loggedIn: false,
-					user: null
-				})
-			}
-		})
-	}
-
-	_login(username, password) {
-		axios
-			.post('/auth/login', {
-				username,
-				password
-			})
-			.then(response => {
-				console.log(response)
-				if (response.status === 200) {
-					// update the state
-					this.setState({
-						loggedIn: true,
-						user: response.data.user
-					})
-				}
-			})
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<Header user={this.state.user} />
-				{/* LINKS to our different 'pages' */}
-				<DisplayLinks _logout={this._logout} loggedIn={this.state.loggedIn} />
-				{/*  ROUTES */}
-				{/* <Route exact path="/" component={Home} /> */}
-				<Route exact path="/" render={() => <Home user={this.state.user} />} />
-				<Route
-					exact
-					path="/login"
-					render={() =>
-						<LoginForm
-							_login={this._login}
-							_googleSignin={this._googleSignin}
-						/>}
-				/>
-				<Route exact path="/signup" component={SignupForm} />
-				{/* <LoginForm _login={this._login} /> */}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="app-container">
+        <NavBar />
+        <Home />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
