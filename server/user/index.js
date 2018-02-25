429ce95db2c7bb4ffee99dfd12011c81f967e6c1
Dylan <dmccapes@mac.com>
2018-02-24 18:34:04 -0800

    Remove user/addworkplace

--- server/user/index.js
+++ server/user/index.js
@@ -5,14 +5,5 @@ const Workplace = require('../db/models/workplace');
 
 // ####### THIS IS WHERE USER CONNECTS TO PROFILE #####
 
-router.patch('/addworkplace', (req, res) => {
-  const { user } = req.body;
-  User.findOneAndUpdate({'_id': user._id},
-    {$set: {workplace: user.workplace}}, {new: true}, (err, doc) => {
-      if (err) console.log(err);
-      console.log(doc);
-    }
-  );
-});
 
 module.exports = router;
